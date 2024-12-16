55d54bddd763f71a24a65572f70c41e8c87ec242
Britney Choimah <bchoimah@gmail.com>
2024-12-15 23:02:12 -0600

    Completed other page layouts

---
+++ main.js
@@ -0,0 +1,6 @@
+const findRecipieBtn = document.querySelector("#findRecipieBtn");
+
+findRecipieBtn.addEventListener("click", () => {
+  window.location.href = "recipie.html";
+  console.log("click");
+});
---
+++ recipie.js
@@ -0,0 +1,5 @@
+const restartBtn = document.querySelector("#restartBtn");
+
+restartBtn.addEventListener("click", () => {
+  window.location.href = "index.html";
+});
