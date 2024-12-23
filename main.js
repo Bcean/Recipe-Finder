5243c2da4811a6d599e5e0cb212d1c246394683f
Britney Choimah <bchoimah@gmail.com>
2024-12-22 19:48:58 -0600

    Random recipie generation and proper fetching of api data

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 const findRecipieBtn = document.querySelector("#findRecipieBtn");
 
 findRecipieBtn.addEventListener("click", () => {
-  window.location.href = "recipie.html";
-  console.log("click");
+  //fetch api
+  console.log("fetching api");
+  const mealApi = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+  fetch(mealApi)
+    .then((response) => {
+      if (!response.ok) {
+        throw error(
+          `Theres been a problem with the http request ${response.status}`
+        );
+        alert(
+          "Theres been an issue with your request please wait or try again later."
+        );
+      }
+      return response.json();
+    })
+    .then((data) => {
+      //localStorage.setItem("mealName", data.meals[0].strMeal)
+
+      console.log(data); //store meal data
+      //title
+      console.log(data.meals[0].strMeal);
+      localStorage.setItem("meal name", data.meals[0].strMeal);
+      //link
+      console.log(data.meals[0].strSource);
+      localStorage.setItem("recipie source", data.meals[0].strSource);
+
+      console.log(data.meals[0].strMealThumb);
+      localStorage.setItem("recipieImgUrl", data.meals[0].strMealThumb);
+      window.location.href = "recipie.html";
+    })
+    .catch((error) => {
+      console.error(`Theres been an error ${error}`);
+      alert(
+        "Theres been an issue with your request please wait or try again later."
+      );
+    });
 });
--- recipie.js
+++ recipie.js
@@ -1,5 +1,22 @@
+const recipieName = localStorage.getItem("meal name");
+const recipieSrc = localStorage.getItem("recipie source");
+const recipieImgUrl = localStorage.getItem("recipieImgUrl");
+
 const restartBtn = document.querySelector("#restartBtn");
+const mealTitleElement = document.querySelector("#mealName");
+const mealImgElement = document.querySelector("#mealImg");
+const recipieBtn = document.querySelector("#dropDownBtn");
 
 restartBtn.addEventListener("click", () => {
   window.location.href = "index.html";
 });
+
+mealTitleElement.textContent = recipieName;
+mealImgElement.src = `${recipieImgUrl}`;
+
+recipieBtn.addEventListener("click", () => {
+  window.location.href = `${recipieSrc}`;
+});
+console.log(recipieName);
+console.log(recipieSrc);
+console.log(recipieImgUrl);
